ac5bf41ab3094ddbbc5c955430d6ef0ed7207de6
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-06-29 09:18:00 +0200

    Use port number from .env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goliath-backend
 
 go 1.22
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Listening on port " + port)
+
 	http.HandleFunc("/hello-world", helloWorldHandler)
 
-	fmt.Println("Listening on port 8080")
-
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
